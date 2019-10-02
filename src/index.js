c31c8f87771cbb2ab582ea88e398fa3173f9c7fd
ViktorGuschin <groftblr@gmail.com>
2019-10-02 23:02:22 +0300

    feat: add brackets test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  function checkBracets(str, bracketsConfig) {
+    if (str.length % 2 !== 0) {
+      return false;
+    }
+    const arrBrackets = bracketsConfig.map(el => el[0] + el[1]);
+    while (str.length !== 0) {
+      let hasBracket = false;
+      for (let element of arrBrackets) {
+        hasBracket = str.indexOf(element) !== -1;
+        if (hasBracket) {
+          break;
+        }
+      }
+      if (!hasBracket && str.length !== 0) {
+        return false;
+      }
+      arrBrackets.forEach(element => {
+        str = str.split(element).join('');
+      });
+    }
+    return true;
+  }
+  if (!checkBracets(expr, ['(', ')'])) {
+    throw new Error('ExpressionError: Brackets must be paired');
+  }
+  // write your solution here
 }
 
 module.exports = {
-    expressionCalculator
-}
+  expressionCalculator,
+};
