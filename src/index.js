e89e54c19506a00184fa07676d70786603f446f7
ViktorGuschin <groftblr@gmail.com>
2019-10-04 17:53:53 +0300

    feat: add convert string expr to polska notation array

--- src/index.js
+++ src/index.js
@@ -4,34 +4,76 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-  function checkBracets(str, bracketsConfig) {
-    if (str.length % 2 !== 0) {
-      return false;
-    }
-    const arrBrackets = bracketsConfig.map(el => el[0] + el[1]);
-    while (str.length !== 0) {
-      let hasBracket = false;
-      for (let element of arrBrackets) {
-        hasBracket = str.indexOf(element) !== -1;
-        if (hasBracket) {
-          break;
-        }
+  // function checkBracets(str, bracketsConfig) {
+  //   if (str.length % 2 !== 0) {
+  //     return false;
+  //   }
+  //   const arrBrackets = bracketsConfig.map(el => el[0] + el[1]);
+  //   while (str.length !== 0) {
+  //     let hasBracket = false;
+  //     for (let element of arrBrackets) {
+  //       hasBracket = str.indexOf(element) !== -1;
+  //       if (hasBracket) {
+  //         break;
+  //       }
+  //     }
+  //     if (!hasBracket && str.length !== 0) {
+  //       return false;
+  //     }
+  //     arrBrackets.forEach(element => {
+  //       str = str.split(element).join('');
+  //     });
+  //   }
+  //   return true;
+  // }
+  // if (!checkBracets(expr, ['(', ')'])) {
+  //   throw new Error('ExpressionError: Brackets must be paired');
+  // }
+
+  const priority = {
+    '*': 2,
+    '/': 2,
+    '+': 1,
+    '-': 1,
+  };
+  let output = [];
+  let stack = [];
+  const arrExpr = expr.split(' ').filter(el => el !== '');
+
+  arrExpr.map((el, index) => {
+    if (el === '(') {
+      stack.push(el);
+    } else if (el === ')') {
+      while (stack.length > 0 && stack[stack.length - 1] !== '(') {
+        output.push(stack.pop());
       }
-      if (!hasBracket && str.length !== 0) {
-        return false;
+      if (stack.length === 0) {
+        throw new Error('ExpressionError: Brackets must be paired');
       }
-      arrBrackets.forEach(element => {
-        str = str.split(element).join('');
-      });
+      stack.pop();
+    } else if (Object.keys(priority).indexOf(el) !== -1) {
+      while (priority[stack[stack.length - 1]] >= priority[el]) {
+        output.push(stack.pop());
+      }
+      stack.push(el);
+    } else {
+      output.push(el);
     }
-    return true;
+  });
+  while (stack.length > 0) {
+    if (Object.keys(priority).indexOf(stack[stack.length - 1]) === -1) {
+      throw new Error('ExpressionError: Brackets must be paired');
+    }
+    output.push(stack.pop());
   }
-  if (!checkBracets(expr, ['(', ')'])) {
-    throw new Error('ExpressionError: Brackets must be paired');
-  }
-  // write your solution here
+
+  return 0;
 }
 
-module.exports = {
-  expressionCalculator,
-};
+// module.exports = {
+//   expressionCalculator,
+// };
+
+console.log(
+  expressionCalculator(' 72 * 95 + 53 + (  2 + 76 - 52 / 1 - 47  ) '),
+);
